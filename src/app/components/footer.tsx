9b180a307d2cef35fef5018868f3ed62cc5a7c3d
Kai Moly <>
2024-11-09 22:34:57 +0900

    個人情報を環境変数に

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="text-center py-6 bg-[#fff7f0] text-[#7d5a50] mt-12 border-t border-gray-200">
+        <footer className="text-center py-6 bg-[#fff7f0] text-[#7d5a50] mt-6 border-t border-gray-200">
             © 2025 All rights reserved.
         </footer>
     );
--- src/app/components/main.tsx
+++ src/app/components/main.tsx
@@ -7,20 +7,30 @@ import dayjs from 'dayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { Button } from "@mui/material";
+import { useState} from 'react';
 
 const Main = () => {
+  const [dateText, setDateText] = useState(process.env.NEXT_PUBLIC_DATE_YEAR + "年" +  process.env.NEXT_PUBLIC_DATE_MONTH + "月" + process.env.NEXT_PUBLIC_DATE_DAY + "日"|| '');
+  const [timeCeremony, setTimeCeremonyText] = useState(process.env.NEXT_PUBLIC_CEREMONY_TIME || '');
+  const [timeReception, setTimeReceptionText] = useState(process.env.NEXT_PUBLIC_RECEPTION_TIME || '');
+  const [churchName, setchurchNameText] = useState(process.env.NEXT_PUBLIC_CHURCH_NAME || '');
+  const [churchAddress, setchurchAddressText] = useState(process.env.NEXT_PUBLIC_CHURCH_ADDRESS || '');
+  const [churchAccess, setchurchAccessText] = useState(process.env.NEXT_PUBLIC_CHURCH_ACCESS || '');
+  const [churchTel, setchurchTelText] = useState(process.env.NEXT_PUBLIC_CHURCH_TEL || '');
+  const [churchMap, setchurchMapText] = useState(process.env.NEXT_PUBLIC_CHURCH_MAP_URL || '');
+
   return (
-    <div className="mt-32 min-h-screen pb-16"> {/* Headerとの余白とFooterのスペースを確保 */}
+    <div className="mt-16 min-h-screen pb-16"> {/* Headerとの余白とFooterのスペースを確保 */}
       <div className="flex flex-col justify-center bg-white opacity-85 relative px-4 sm:px-6 md:px-16 lg:px-24 xl:px-32 max-w-4xl mx-auto">
         <section className="my-6 sm:my-12 text-center">
           <h2 className="text-3xl sm:text-4xl font-serif text-[#7d5a50] mb-6">ご挨拶</h2>
           <p className="text-base sm:text-lg leading-relaxed break-words">
-            謹啓　〇〇の候（時候の挨拶）皆様にはますますご清祥のこととお慶び申し上げます
-            このたび　私たちは結婚式を挙げることになりました
-            つきましては　日ごろお世話になっている皆様に お集まりいただき
-            ささやかな披露宴を催したいと存じます
-            ご多用中　誠に恐縮ではございますが
-            ご来臨の栄を賜りたく　謹んでご案内申し上げます
+          拝啓<br/>
+          晩秋の候、皆様にはますますご健勝のこととお慶び申し上げます<br/>
+このたび、私たちは{dateText}に結婚式を挙げる運びとなりました<br/>
+これまで私たちを支えていただきました皆様へ、日頃の感謝の気持ちをお伝えし、心に残るひとときを共に過ごしたく存じます<br/>
+つきましては、誠に勝手ながら下記により挙式および披露宴を執り行いますので、ご多用中とは存じますが、ぜひご出席賜りますようお願い申し上げます<br/>
+敬具
           </p>
           <h2 className="text-3xl sm:text-4xl font-serif text-[#7d5a50] mt-12 mb-6">お願い</h2>
           <p className="text-base sm:text-lg leading-relaxed break-words mb-4">
@@ -32,7 +42,7 @@ const Main = () => {
             </Button>
           </div>
           <p className="text-base sm:text-lg leading-relaxed break-words mb-4">
-            まだ友達追加されていない方は、こちらから結婚式公式Lineアカウントを友達追加を行なってください。
+            まだ友達追加されていない方は、こちらから結婚式公式Lineアカウントの友達追加を行なってください。
           </p>
           <div className='flex justify-center pb-6'>
             <Button sx={{ backgroundColor: '#06C755', color: '#FFFFFF' }} variant="contained" href={process.env.NEXT_PUBLIC_LINE_URL}>
@@ -42,21 +52,30 @@ const Main = () => {
         </section>
 
         <section className="my-12 text-center">
-          <h2 className="text-3xl sm:text-4xl font-serif text-[#7d5a50] mb-6">スケジュール</h2>
+          <h2 className="text-3xl sm:text-4xl font-serif text-[#7d5a50] mb-6">日時</h2>
           <p className="text-base sm:text-lg leading-relaxed mb-4 break-words">
-            結婚式の日程はこちらです。
+            結婚式の予定はこちらです
           </p>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <div className='flex justify-center pt-4 sm:pt-6'>
               <DemoContainer components={['DatePicker']}>
-                <DatePicker label="挙式日" defaultValue={dayjs('2025-04-17')} />
+                <DatePicker label="挙式日" defaultValue={dayjs(process.env.NEXT_PUBLIC_DATE_YEAR + '-' + process.env.NEXT_PUBLIC_DATE_MONTH + '-' + process.env.NEXT_PUBLIC_DATE_DAY)} />
               </DemoContainer>
             </div>
           </LocalizationProvider>
+          挙式: {timeCeremony}<br/>
+          披露宴: {timeReception}
           <h2 className="text-3xl sm:text-4xl font-serif text-[#7d5a50] mt-12 mb-6">場所</h2>
           <p className="text-base sm:text-lg leading-relaxed break-words">
-            結婚式の場所はこちらです。
+            結婚式場はこちらです<br/>
+会場: {churchName}<br/>
+所在地: {churchAddress}<br/>
+アクセス: {churchAccess}<br/>
+電話番号: {churchTel}
           </p>
+          <div className='flex justify-center pb-6'>
+          <iframe src={churchMap} width="600" height="450"  loading="lazy" ></iframe>
+          </div>
         </section>
       </div>
     </div>
