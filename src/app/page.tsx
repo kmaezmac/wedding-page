aa80e93b9878a50f0d52e80bb898ffe8f317135f
Kai Moly <>
2025-04-26 17:59:37 +0900

    faviconの設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 // import Header from './components/header'
 import Main from './components/main'
 // import Footer from './components/footer'
+import Head from 'next/head'
 
 
 const page = () => {
@@ -9,9 +10,10 @@ const page = () => {
     <>
         <div className="bg-[url('./components/background.jpeg')] w-full  bg-cover bg-center bg-no-repeat">
         <div className="container mx-auto px-4 py-6">
-        <head>
+        <Head>
         <meta name="robots" content="noindex" />
-      </head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        </Head>
         {/* <Header/> */}
         <Main/>
         {/* <Footer/> */}
